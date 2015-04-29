d139c84ee645c50c702ef76b3d50c6c216968f79
Holger Burbach <holger@burbach.eu>
2015-04-29 19:51:26 +0200

    initial project setup

---
+++ src/main/java/com/github/simpleimapsorter/sorter/Sorter.java
@@ -0,0 +1,13 @@
+package com.github.simpleimapsorter.sorter;
+
+public class Sorter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
